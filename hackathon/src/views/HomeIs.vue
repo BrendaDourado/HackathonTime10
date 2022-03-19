80e597d7ba50ecc78ecdc6d8f841ee306aa19209
Mythalie Sandretti <s.mythalie@hotmail.com>
2022-03-19 17:11:15 -0300

    Alteração

--- hackathon/src/views/HomeIs.vue
+++ hackathon/src/views/HomeIs.vue
@@ -6,31 +6,32 @@
     <p class="text-home text-justify mt-5 ml-4 mr-4">
       Com a chegada da páscoa, ficamos muito impressionados com a variedade de
       ovos que surgem com a data. Por isso, resolvemos trazer para você os
-      melhores ovos de Páscoa.
+      melhores ovos caseiros de Páscoa. Comprar de pequenos produtores é uma
+      ideia que contribui para a economia e ajuda quem mais precisa. Faça parte
+      dessa idéia, divulgue seu produto!
     </p>
-    <v-btn @click="delay" color="blue" class="mr-4" type="submit"> Clique Aqui </v-btn>
+    <v-btn @click="delay" color="blue" class="mr-4" type="submit">
+      Clique Aqui
+    </v-btn>
   </v-container>
 </template>
 
 <script>
 export default {
   data: () => ({
-     name: "HomeIs",
+    name: "HomeIs",
   }),
-methods: {
+  methods: {
     submit() {
       this.$refs.observer.validate();
-      
     },
-      delay(){
-        setTimeout(()=>{
-          this.$router.push('/cadastrocliente')
-          
-          },100)
-
+    delay() {
+      setTimeout(() => {
+        this.$router.push("/cadastrocliente");
+      }, 100);
+    },
   },
-}
-}
+};
 </script>
 
 <style>
