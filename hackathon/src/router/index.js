0f7a818c3bcf3692b195ee0d796b2ff63850d125
Mythalie Sandretti <s.mythalie@hotmail.com>
2022-03-19 14:01:05 -0300

    Criando views

--- hackathon/src/router/index.js
+++ hackathon/src/router/index.js
@@ -1,14 +1,35 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomeIs from "../views/HomeIs.vue"
+import CadastroEmpresa from "../views/CadastroEmpresa.vue";
+import FormCliente from "../views/FormCliente.vue";
 
+Vue.use(VueRouter);
 
-Vue.use(VueRouter)
-
- 
+const routes = [ 
+  {
+    path: "/home",
+    name: "Home",
+    component: HomeIs,
   
+  },
+  {
+  path: "/cadastroempresa",
+  name: "Cadastro de Empresa",
+  component: CadastroEmpresa,
+},
+{
+  path: "/formcliente",
+  name: "Formulário Cliente",
+  component: FormCliente,
+},
+]
 
 const router = new VueRouter({
-  //routes
-})
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
 
-export default router
+
+export default router;
