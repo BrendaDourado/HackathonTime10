95cd8645a2d0f5a72fe5f166333a53f085bdfc39
Brenda Paulla Dourado de Oliveira <brendapdourado@gmail.com>
2022-03-19 16:29:49 -0300

    pages de Analise

--- hackathon/src/router/index.js
+++ hackathon/src/router/index.js
@@ -3,15 +3,9 @@ import VueRouter from "vue-router";
 import HomeIs from "../views/HomeIs.vue"
 import CadastroEmpresa from "../views/CadastroEmpresa.vue";
 import CadastroCliente from "../views/CadastroCliente.vue";
-<<<<<<< HEAD
-/*import AnaliseEmpresa from "../views/AnaliseEmpresa.vue";
+import AnaliseEmpresa from "../views/AnaliseEmpresa.vue";
 import AnaliseSugestao from "../views/AnaliseSugestao.vue";
-import OvosLista from "../views/OvosLista.vue";*/
-=======
-//import AnaliseEmpresa from "../views/AnaliseEmpresa.vue";
-//import AnaliseSugestao from "../views/AnaliseSugestao.vue";
 import OvosLista from "../views/OvosLista.vue";
->>>>>>> 0dcf23acb9518d96441f38c2a7b9ef18e834ba81
 
 Vue.use(VueRouter);
 
@@ -32,25 +26,21 @@ const routes = [
   name: "Formulário Cliente",
   component: CadastroCliente,
 },
-<<<<<<< HEAD
-/*
-=======
-//{
-//  path: "/analiseempresa",
-//  name: "Análise Empresa",
-//  component: AnaliseEmpresa,
-//},
-//{
-//  path: "/analisesugestao",
-//  name: "Análise Sugestão",
-//  component: AnaliseSugestao,
-//}, 
->>>>>>> 0dcf23acb9518d96441f38c2a7b9ef18e834ba81
+{
+path: "/analiseempresa",
+name: "Análise Empresa",
+ component: AnaliseEmpresa,
+},
+{
+ path: "/analisesugestao",
+ name: "Análise Sugestão",
+ component: AnaliseSugestao,
+}, 
 {
   path: "/OvosLista",
   name: "Lista de Ovos",
   component: OvosLista,
-},*/
+},
 ]
 
 const router = new VueRouter({
---
+++ hackathon/src/views/AnaliseEmpresa.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+    <h2 class="text-h5 text-center mb-3 mt-5">OBRIGADA! </h2>
+    <img src="https://1.bp.blogspot.com/-5p8KNBdb5WQ/UVcmVt-QG1I/AAAAAAAAB1w/3oVpwwLs_jY/s1600/53.gif" alt=""/>
+    <h3> Muito obrigada por querer fazer parte dessa ideia, o seu cadastro está em análise, assim que for aprovado você poderá compartilhar para os clientes os seus produtos ! </h3>
+    </div>
+</template>
+<script>
+export default {
+    created(){
+
+    }
+}
+</script>
+<style scoped>
+img {
+    width: 80%;
+}
+</style>
