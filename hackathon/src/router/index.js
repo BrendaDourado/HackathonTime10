0dcf23acb9518d96441f38c2a7b9ef18e834ba81
perifadev <rnznmc@gmail.com>
2022-03-19 16:18:23 -0300

    Adicionando as lojas

--- hackathon/src/router/index.js
+++ hackathon/src/router/index.js
@@ -3,8 +3,8 @@ import VueRouter from "vue-router";
 import HomeIs from "../views/HomeIs.vue"
 import CadastroEmpresa from "../views/CadastroEmpresa.vue";
 import CadastroCliente from "../views/CadastroCliente.vue";
-import AnaliseEmpresa from "../views/AnaliseEmpresa.vue";
-import AnaliseSugestao from "../views/AnaliseSugestao.vue";
+//import AnaliseEmpresa from "../views/AnaliseEmpresa.vue";
+//import AnaliseSugestao from "../views/AnaliseSugestao.vue";
 import OvosLista from "../views/OvosLista.vue";
 
 Vue.use(VueRouter);
@@ -26,18 +26,18 @@ const routes = [
   name: "Formulário Cliente",
   component: CadastroCliente,
 },
+//{
+//  path: "/analiseempresa",
+//  name: "Análise Empresa",
+//  component: AnaliseEmpresa,
+//},
+//{
+//  path: "/analisesugestao",
+//  name: "Análise Sugestão",
+//  component: AnaliseSugestao,
+//}, 
 {
-  path: "/analiseempresa",
-  name: "Análise Empresa",
-  component: AnaliseEmpresa,
-},
-{
-  path: "/analisesugestao",
-  name: "Análise Sugestão",
-  component: AnaliseSugestao,
-},
-{
-  path: "/ovoslista",
+  path: "/OvosLista",
   name: "Lista de Ovos",
   component: OvosLista,
 },
