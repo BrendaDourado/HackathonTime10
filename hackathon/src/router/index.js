73b03dabe38462ab2e7eaf9c4f09d5774f2e4d76
Mythalie Sandretti <s.mythalie@hotmail.com>
2022-03-19 16:37:52 -0300

    Alterações finais home

--- hackathon/src/router/index.js
+++ hackathon/src/router/index.js
@@ -3,9 +3,9 @@ import VueRouter from "vue-router";
 import HomeIs from "../views/HomeIs.vue"
 import CadastroEmpresa from "../views/CadastroEmpresa.vue";
 import CadastroCliente from "../views/CadastroCliente.vue";
-import AnaliseEmpresa from "../views/AnaliseEmpresa.vue";
-import AnaliseSugestao from "../views/AnaliseSugestao.vue";
-import OvosLista from "../views/OvosLista.vue";
+// import AnaliseEmpresa from "../views/AnaliseEmpresa.vue";
+// import AnaliseSugestao from "../views/AnaliseSugestao.vue";
+// import OvosLista from "../views/OvosLista.vue";
 
 Vue.use(VueRouter);
 
@@ -17,30 +17,30 @@ const routes = [
 
   },
   {
-  path: "/cadastroempresa",
-  name: "Cadastro de Empresa",
-  component: CadastroEmpresa,
-},
-{
-  path: "/cadastrocliente",
-  name: "Formulário Cliente",
-  component: CadastroCliente,
-},
-{
-  path: "/analiseempresa",
-  name: "Análise Empresa",
-  component: AnaliseEmpresa,
-},
-{
-  path: "/analisesugestao",
-  name: "Análise Sugestão",
-  component: AnaliseSugestao,
-},
-{
-  path: "/ovoslista",
-  name: "Lista de Ovos",
-  component: OvosLista,
-},
+    path: "/cadastroempresa",
+    name: "Cadastro de Empresa",
+    component: CadastroEmpresa,
+  },
+  {
+    path: "/cadastrocliente",
+    name: "Formulário Cliente",
+    component: CadastroCliente,
+  },
+  // {
+  //   path: "/analiseempresa",
+  //   name: "Análise Empresa",
+  //   component: AnaliseEmpresa,
+  // },
+  // {
+  //   path: "/analisesugestao",
+  //   name: "Análise Sugestão",
+  //   component: AnaliseSugestao,
+  // },
+  // {
+  //   path: "/ovoslista",
+  //   name: "Lista de Ovos",
+  //   component: OvosLista,
+  // },
 ]
 
 const router = new VueRouter({
--- hackathon/src/views/HomeIs.vue
+++ hackathon/src/views/HomeIs.vue
@@ -8,6 +8,19 @@
       ovos que surgem com a data. Por isso, resolvemos trazer para você os
       melhores ovos de Páscoa.
     </p>
+    <div class="form">
+      <v-form>
+        <v-row>
+          <v-col cols="12" sm="12">
+            <v-text-field
+              v-model="first"
+              label="Pesquisar produto"
+              solo
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-form>
+    </div>
   </v-container>
 </template>
 
@@ -19,12 +32,16 @@ export default {
 
 <style>
 .home-img {
-     display: flex;
-     justify-content: center;
-     max-width: 100%;
-     height: 300px;
+  display: flex;
+  justify-content: center;
+  max-width: 100%;
+  height: 300px;
 }
 .text-home {
   font-size: 14px;
+}
+.form {
+  display: flex;
+  justify-content: center;
 }
 </style>
